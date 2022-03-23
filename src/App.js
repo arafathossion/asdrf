ea3914fa8c8dc7e03b44129fc55da15cface2a1d
arafathossion <arafathossionrahman@gmail.com>
2022-03-23 09:03:33 +0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
             <Banner name='Arafat' para='I want to do something mainblooing for major people' country='My Country Name is bangladesh'></Banner>
             <Banner name="Anis" para="For this I try my best but still I can't do that."></Banner>
         </div>
+        
     );
 }
 
